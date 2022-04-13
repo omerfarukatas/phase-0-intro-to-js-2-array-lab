f4c577c6cbff630b0444a3890ed31242568c9287
Omer <omerfarukatas96@gmail.com>
2022-04-14 01:27:56 +0300

    3 tests left

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift();
+}
+
+// function appendCat(name){
+//     return cats.concat(name);
+// }
+
+// function appendCat(name){
+//     return newCats = [...cats, name];
+// }
+
+function appendCat(name){
+    return [...cats, name];
+}
+
+
+
