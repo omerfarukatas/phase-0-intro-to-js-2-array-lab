dfc7c2e82d558077029e29b852b98316b8431d55
Omer <omerfarukatas96@gmail.com>
2022-04-16 09:58:50 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -10,25 +10,26 @@ function destructivelyPrependCat(name){
     cats.unshift(name);
 }
 
-function destructivelyRemoveLastCat(name){
+function destructivelyRemoveLastCat(){
     cats.pop();
 }
 
-function destructivelyRemoveFirstCat(name){
+function destructivelyRemoveFirstCat(){
     cats.shift();
 }
-
-// function appendCat(name){
-//     return cats.concat(name);
-// }
-
-// function appendCat(name){
-//     return newCats = [...cats, name];
-// }
 
 function appendCat(name){
     return [...cats, name];
 }
 
+function prependCat(name){
+    return [name, ...cats];
+}
 
+function removeLastCat(){
+    return cats.slice(0, 2);
+}
 
+function removeFirstCat(){
+    return cats.slice(1);
+}
